48d336caa7db1cb3e5aed0256ff150aa639ffebe
Gage <gage@rubyshore.com>
2023-05-31 10:12:43 -0500

    Added pages controller & defined home action & created home view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+ 
+  # 1) when defining the route RAILS knows your sending this to a controller SO 'application_controller' is not necessary
+  #     you can just say 'application' and it knows it is a controller.
+  # 2) the #hello is referring to a method named 'hello' in the application controller.
+  # 3) The action 'hello' could not be found for ApplicationController == the error message
+  #     displayed when the action is not defined
+  # route 'controller#action'
+  root 'pages#home'
+  
 end
